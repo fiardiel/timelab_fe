3b88a0b2f791fed90d321ff10d4a8a1e306c12fb
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2025-07-25 15:08:58 +1000

    [FEAT] added login page and middleware

---
+++ lib/auth/login.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { LoginForm } from "@/types/auth/login";
+import { cookies } from "next/headers";
+
+export async function handleLoginFormSubmit(data: LoginForm): Promise<{ ok: boolean; message: string }> {
+  const reqUrl = `${process.env.API_BASE_URL}/api/login/`;
+  try {
+    const res = await loginRequest(reqUrl, data);
+
+    if (res.ok) {
+      await setRefreshAndAccessTokens(res);
+      return { ok: true, message: "Login successful" };
+    }
+
+    const errorResponse = await res.json();
+    return { ok: false, message: errorResponse.detail };
+  } catch (error) {
+    return { ok: false, message: "An error occured while logging in" };
+  }
+}
+
+async function loginRequest(url: string, data: LoginForm): Promise<Response> {
+  return fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+}
+
+async function setRefreshAndAccessTokens(response: Response) {
+  const { refresh, access } = await response.json();
+  cookies().set("refreshToken", refresh, { path: "/", httpOnly: true });
+  cookies().set("accessToken", access, { path: "/", httpOnly: true });
+}
---
+++ middleware.ts
@@ -0,0 +1,66 @@
+import { NextRequest, NextResponse } from "next/server"
+
+const API_BASE_URL = process.env.API_BASE_URL;
+
+const validateAccessToken = async (accessToken?: string) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/auth/validate/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    return response.ok;
+
+  } catch (error) {
+    return false;
+  }
+}
+
+const refreshAccessToken = async (refreshToken?: string) => {
+  let response: Response;
+
+  try {
+    response = await fetch(`${API_BASE_URL}/api/token/refresh`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refresh: refreshToken }),
+    })
+  } catch (error) {
+    return null;
+  }
+
+  if (!response.ok) {
+    return null;
+  }
+
+  const data = await response.json();
+  return data.access;
+}
+
+export async function middleware(req: NextRequest) {
+  const accessToken = req.cookies.get("accessToken")?.value;
+  const refreshToken = req.cookies.get("refreshToken")?.value;
+
+  let isValid = await validateAccessToken(accessToken);
+
+  if (!isValid) {
+    const newAccessToken = await refreshAccessToken(refreshToken);
+    if (!newAccessToken) {
+      return NextResponse.redirect(new URL("/auth/login", req.url))
+    }
+    const response = NextResponse.next();
+    response.cookies.set("accessToken", newAccessToken, { path: "/", httpOnly: true });
+    return response
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!auth|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
